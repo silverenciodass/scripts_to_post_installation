f7635a8b44fbf9dde53d9966e1d58499e17daf3d
Ulisses Cardoso <rasracapur@outlook.com>
2022-07-13 22:16:17 -0300

    modified:   scripts/asdf_install.sh -------------------------------------------- install languages

--- scripts/asdf_install.sh
+++ scripts/asdf_install.sh
@@ -27,3 +27,20 @@ asdf plugin add sml  https://github.com/asdf-community/asdf-sml.git
 asdf plugin add terraform  https://github.com/asdf-community/asdf-hashicorp.git
 asdf plugin add vault  https://github.com/asdf-community/asdf-hashicorp.git
 
+asdf install ninja  1.11.0
+asdf install erlang  25.0.2
+asdf install cmake  3.23.2
+asdf install crystal  1.5.0
+asdf install deno  1.23.3
+asdf install dotnet-core  6.0.301
+asdf install elixir  master-otp-24
+asdf install haskell  9.2.3
+asdf install julia  1.7.3
+asdf install kotlin  1.7.10
+asdf install meson  0.63.0
+asdf install nodejs  lts
+asdf install ruby  3.1.2
+asdf install rust  1.62.0
+asdf install sml  110.99
+asdf install terraform  1.2.4
+asdf install vault  1.11.0
