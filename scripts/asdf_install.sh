6495400add91c73a7abea9397af8938e98f7e415
Ulisses Cardoso <rasracapur@outlook.com>
2022-07-13 22:09:58 -0300

    modified:   scripts/asdf_install.sh ---------------------------------------------- clone and source asdf

--- scripts/asdf_install.sh
+++ scripts/asdf_install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 
+bash_file="$HOME/.bashrc"
+
+echo ". $HOME/.asdf/asdf.sh" >> bash_file
+echo ". $HOME/.asdf/completions/asdf.bash" >> bash_file
+
+source bash_file
