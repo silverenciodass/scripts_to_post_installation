79c64f770bd18acad0e6d0b7a9920569f5859ed9
Ulisses Cardoso <rasracapur@outlook.com>
2022-07-13 21:39:30 -0300

    modified:   pyenv_install.sh ------------------------------------------ clone and build pyenv

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -2,3 +2,12 @@
 
 # install dependencies for python build
 sudo pacman -S --needed git base-devel openssl zlib xz tk
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+cd ~/.pyenv && src/configure && make -C src
+
+cd $HOME
+
+
+
