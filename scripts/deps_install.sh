9b9d23141a0841774af6506c26282f52549ae8c3
Ulisses Cardoso <rasracapur@outlook.com>
2022-07-13 22:01:44 -0300

    modified:   scripts/deps_install.sh --------------------------------------- add packages to install

--- scripts/deps_install.sh
+++ scripts/deps_install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-
+sudo pacman -Syu
+sudo pacman -S --needed base-devel libffi libyaml openssl zlib ncurses glu mesa
+sudo pacman -S --needed wxgtk2 libpng libssh unixodbc libxslt fop zip unzip p7zip wget rsync
