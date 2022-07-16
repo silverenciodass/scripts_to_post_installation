6c9c0aed61359b620ac9accf8fdf358d81243653
Ulisses Cardoso <rasracapur@outlook.com>
2022-07-16 02:23:28 -0300

    modified:   deps_install.sh ------------------------------------------------ removed wxgtk2 to installation packages

--- scripts/deps_install.sh
+++ scripts/deps_install.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syu
 sudo pacman -S --needed base-devel libffi libyaml openssl zlib ncurses glu mesa
-sudo pacman -S --needed wxgtk2 libpng libssh unixodbc libxslt fop zip unzip p7zip wget rsync
+sudo pacman -S --needed  libpng libssh unixodbc libxslt fop zip unzip p7zip wget rsync
 
 # if using apt
 # sudo apt update
