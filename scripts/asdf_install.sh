13707b26971ad75556f6df6e678af64643cfe650
Ulisses Cardoso <rasracapur@outlook.com>
2022-07-18 00:39:45 -0300

    modified:   scripts/asdf_install.sh --------------------------------------------- change bash_file to $bash_file

--- scripts/asdf_install.sh
+++ scripts/asdf_install.sh
@@ -4,10 +4,10 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 
 bash_file="$HOME/.bashrc"
 
-echo ". $HOME/.asdf/asdf.sh" >> bash_file
-echo ". $HOME/.asdf/completions/asdf.bash" >> bash_file
+echo ". $HOME/.asdf/asdf.sh" >> $bash_file
+echo ". $HOME/.asdf/completions/asdf.bash" >> $bash_file
 
-source bash_file
+source $bash_file
 
 asdf plugin add cmake  https://github.com/asdf-community/asdf-cmake.git
 asdf plugin add crystal  https://github.com/asdf-community/asdf-crystal.git
